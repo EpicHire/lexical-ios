da93f77bc2d97ff0b840e12c067c6aa1183928f0
Kevin Barnes <kevin@donniebarnes.com>
2024-12-06 11:06:44 -0500

    Depend on forked swift-markdown

--- Plugins/LexicalMarkdown/LexicalMarkdown/LexicalMarkdownSupport.swift
+++ Plugins/LexicalMarkdown/LexicalMarkdown/LexicalMarkdownSupport.swift
@@ -48,16 +48,11 @@ extension Lexical.TextNode: NodeMarkdownInlineSupport {
     }
 
     if format.italic {
-      // TODO (mani) - underline + italic both use Emphasis node
-      // should we create a separate node?
       node = Markdown.Emphasis(node)
     }
 
     if format.underline {
-      // TODO (mani) - underline + italic both use Emphasis node
-      // should we create a separate node?
-      node = Markdown.CustomInline("<u>\(node.plainText)</u>")
-      //node = Markdown.Emphasis(node)
+      node = Markdown.Underline(node)
     }
 
     if format.superScript {
